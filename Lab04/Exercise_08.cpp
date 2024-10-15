6420340cbbcab806b65d5dfd37351c238f0347b8
Pham Viet Hoa <152608093+phamhoa2416@users.noreply.github.com.>
2024-10-15 15:42:17 +0700

    add exercises to Lab04

---
+++ Lab04/Exercise_08.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+template <class T>
+map<T, double> fuzzy_set_union(const map<T, double> &a, const map<T, double> &b)
+{
+    map<T, double> result = a;
+    for (const pair<T, double> &element : b)
+    {
+        if (result.find(element.first) == result.end())
+            result[element.first] = element.second;
+        else
+            result[element.first] = max(result[element.first], element.second);
+    }
+    return result;
+}
+
+template <class T>
+map<T, double> fuzzy_set_intersection(const map<T, double> &a, const map<T, double> &b)
+{
+    map<T, double> result = a;
+    for (const pair<T, double> &element : b)
+    {
+        if (result.find(element.first) != result.end())
+            result[element.first] = min(result[element.first], element.second);
+    }
+    return result;
+}
+
+template <class T>
+void print_fuzzy_set(const std::map<T, double> &fuzzy_set)
+{
+    cout << "{";
+    for (const auto &x : fuzzy_set)
+        std::cout << "(" << x.first << "," << x.second << ") ";
+    cout << "}";
+    std::cout << std::endl;
+}
+
+// int main() {
+//     std::map<int, double> a = {{1, 0.2}, {2, 0.5}, {3, 1}, {4, 0.6}, {5, 0.7}};
+//     std::map<int, double> b = {{1, 0.5}, {2, 0.4}, {4, 0.9}, {5, 0.4}, {6, 1}};
+//     std::cout << "A = "; print_fuzzy_set(a);
+//     std::cout << "B = "; print_fuzzy_set(b);
+//     std::map<int, double> c = fuzzy_set_union(a, b);
+//     std::map<int, double> d = fuzzy_set_intersection(a, b);
+//     std::cout << "Union: "; print_fuzzy_set(c);
+//     std::cout << "Intersection: "; print_fuzzy_set(d);
+
+//     return 0;
+
+// }
---
+++ Lab04/Exercise_09.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+vector<int> dijkstra(const vector<vector<pair<int, int>>> &adj)
+{
+    priority_queue<pair<int, int>> queue;
+    vector<int> distance(adj.size(), INT_MAX);
+
+    distance[0] = 0;
+    queue.push({0, 0});
+
+    while (!queue.empty())
+    {
+        pair<int, int> u_pair = queue.top();
+        queue.pop();
+
+        int u = u_pair.first;
+        for (auto v_pair : adj[u])
+        {
+            int v = v_pair.first;
+            int w = v_pair.second;
+
+            if (distance[v] > distance[u] + w)
+            {
+                distance[v] = distance[u] + w;
+                queue.push({v, distance[v]});
+            }
+        }
+    }
+
+    return distance;
+}
+
+// int main()
+// {
+//     int size = 9;
+//     vector<vector<pair<int, int>>> adj(size);
+
+//     auto add_edge = [&adj](int u, int v, int w)
+//     {
+//         adj[u].push_back({v, w});
+//         adj[v].push_back({u, w});
+//     };
+
+//     add_edge(0, 1, 4);
+//     add_edge(0, 7, 8);
+//     add_edge(1, 7, 11);
+//     add_edge(1, 2, 8);
+//     add_edge(2, 3, 7);
+//     add_edge(2, 8, 2);
+//     add_edge(3, 4, 9);
+//     add_edge(3, 5, 14);
+//     add_edge(4, 5, 10);
+//     add_edge(5, 6, 2);
+//     add_edge(6, 7, 1);
+//     add_edge(6, 8, 6);
+//     add_edge(7, 8, 7);
+
+//     vector<int> distance = dijkstra(adj);
+//     for (int i = 0; i < distance.size(); ++i)
+//     {
+//         cout << "distance " << 0 << "->" << i << " = " << distance[i] << endl;
+//     }
+
+//     return 0;
+// }
